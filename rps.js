5dd849dc3151a686653edce64d79a76c128691b4
David Vessels <97754577+dvessels91@users.noreply.github.com>
2023-08-28 15:15:34 -0400

    Created the random string generator: Rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -1,5 +1,9 @@
+function getComputerChoice (strings) {
+    let randomNumber = Math.floor(Math.random() *  strings.length); 
+    return strings[randomNumber];
+}
 
+const myArray = ['Rock', 'Paper', 'Scissors'];
+let randomItem = getComputerChoice(myArray)
 
-
-
-console.log()
+console.log(randomItem)
